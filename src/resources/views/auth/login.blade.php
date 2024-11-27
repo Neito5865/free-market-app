2c67dc1d413a3dfe0bde58c56f54188bc29bf1d9
NeitoKomiyama <neito.komi11@gmail.com>
2024-11-27 19:14:51 +0900

    create:ログインページ作成

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
+@endsection
+
+@section('content')
+    <div class="login__container">
+        <div class="login__heading">
+            <h2>ログイン</h2>
+        </div>
+        <div class="login__form">
+            <form method="POST" action="">
+                @csrf
+                <div class="login-form__group">
+                    <label class="login-form__label" for="email">メールアドレス</label>
+                    <input class="login-form__input" type="email" name="email" id="email" value="{{ old('email')}}">
+                </div>
+                <div class="login-form__group">
+                    <label class="login-form__label" for="password">パスワード</label>
+                    <input class="login-form__input" type="password" name="password" id="password" value="{{ old('password')}}">
+                </div>
+                <div class="login-form__button">
+                    <input class="login-form__button-submit" type="submit" value="ログインする">
+                </div>
+            </form>
+        </div>
+        <div class="login__link">
+            <a href="">会員登録はこちら</a>
+        </div>
+    </div>
+@endsection
