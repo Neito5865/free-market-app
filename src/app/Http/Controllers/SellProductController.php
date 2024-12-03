d4d2fd1e279c4891d5207bd13bf69dc2da1d1833
NeitoKomiyama <neito.komi11@gmail.com>
2024-12-03 20:55:27 +0900

    edit:出品画面の作成

--- src/app/Http/Controllers/SellProductController.php
+++ src/app/Http/Controllers/SellProductController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Condition;
+use App\Models\Category;
 
 class SellProductController extends Controller
 {
     public function create()
     {
-        return view('items.create');
+        $categories = Category::all();
+        $conditions = Condition::all();
+        return view('items.create', compact('categories', 'conditions'));
+    }
+
+    public function store(Request $request)
+    {
+        // $item->categories()->attach($request->input('categories'));
     }
 }
--- src/resources/views/items/create.blade.php
+++ src/resources/views/items/create.blade.php
@@ -15,6 +15,12 @@
                 <div class="item-create-form__group">
                     <div class="item-create-form__group--content">
                         <label class="item-create-form__label" for="image">商品画像</label>
+                        <div id="preview-container" class="image-preview-area"></div>
+                        <div class="image-preview-container">
+                            <label for="image">
+                                <p>画像を選択する</p>
+                            </label>
+                        </div>
                         <input class="item-create-form__input" type="file" name="image" id="image">
                     </div>
                     <div class="item-create-form__error">
@@ -29,10 +35,17 @@
                 <div class="item-create-form__group">
                     <div class="item-create-form__group--content">
                         <label class="item-create-form__label" for="category">カテゴリー</label>
-                        <input class="item-create-form__input" type="file" name="category" id="category">
+                        <div class="category-selection">
+                            @foreach ($categories as $category)
+                                <label class="category-label">
+                                    <input class="category-input" type="checkbox" name="categories[]" value="{{ $category->id }}">
+                                    <span class="category-name">{{ $category->category }}</span>
+                                </label>
+                            @endforeach
+                        </div>
                     </div>
                     <div class="item-create-form__error">
-                        @error('category')
+                        @error('categories')
                         {{ $message }}
                         @enderror
                     </div>
@@ -40,11 +53,15 @@
                 <div class="item-create-form__group">
                     <div class="item-create-form__group--content">
                         <label class="item-create-form__label" for="condition">商品の状態</label>
-                        <select class="item-create-form__select" name="condition_id" id="condition">
-                            <option value="">選択してください</option>
-                            <option value="">良好</option>
-                            <option value="">テスト</option>
-                        </select>
+                        <div class="condition-select">
+                            <select class="item-create-form__select" name="condition_id" id="condition">
+                                <option value="">選択してください</option>
+                                @foreach ($conditions as $condition)
+                                    <option value="{{ $condition->id }}" {{ old('condition') ? 'selected' : '' }}>{{ $condition->condition }}</option>
+                                @endforeach
+                            </select>
+                            <div class="triangle"></div>
+                        </div>
                     </div>
                     <div class="item-create-form__error">
                         @error('condition_id')
@@ -58,7 +75,7 @@
                 <div class="item-create-form__group">
                     <div class="item-create-form__group--content">
                         <label class="item-create-form__label" for="name">商品名</label>
-                        <input class="item-create-form__input" type="text" name="name" id="name">
+                        <input class="item-create-form__input" type="text" name="name" id="name" value="{{ old('name') }}">
                     </div>
                     <div class="item-create-form__error">
                         @error('name')
@@ -72,12 +89,48 @@
                         <textarea class="item-create-form__textarea" name="description" id="description">{{ old('description') }}</textarea>
                     </div>
                     <div class="item-create-form__error">
-                        @error('name')
+                        @error('description')
                         {{ $message }}
                         @enderror
                     </div>
                 </div>
+                <div class="item-create-form__group">
+                    <div class="item-create-form__group--content">
+                        <label class="item-create-form__label" for="price">販売価格</label>
+                        <input class="item-create-form__input" type="text" name="price" id="price">
+                    </div>
+                    <div class="item-create-form__error">
+                        @error('price')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+                <div class="item-create-form__btn">
+                    <button class="item-create-form__btn--submit" type="submit">出品する</button>
+                </div>
             </form>
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script>
+        document.getElementById('image').addEventListener('change', function(event) {
+            const previewContainer = document.getElementById('preview-container');
+            const file = event.target.files[0];
+
+            if (file && file.type.startsWith('image/')) {
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    previewContainer.innerHTML = '';
+                    const img = document.createElement('img');
+                    img.src = e.target.result;
+                    previewContainer.appendChild(img);
+                };
+                reader.readAsDataURL(file);
+            } else {
+                previewContainer.innerHTML = '';
+            }
+        });
+    </script>
 @endsection
