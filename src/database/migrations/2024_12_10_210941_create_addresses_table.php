63759955713f28701761885f037431aa662f64d0
NeitoKomiyama <neito.komi11@gmail.com>
2024-12-10 21:23:11 +0900

    create:商品購入機能：テーブル作成

---
+++ src/database/migrations/2024_12_10_210941_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 80);
+            $table->string('postCode', 8);
+            $table->string('address');
+            $table->string('building');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
